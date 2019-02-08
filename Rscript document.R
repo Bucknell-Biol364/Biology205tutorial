2023db739bbf882e3e138bc818e47bb0bf4bae44
chadsussman <46733944+chadsussman@users.noreply.github.com>
2019-02-08 17:16:39 -0500

    trying this to commit

--- Rscript document.R
+++ Rscript document.R
@@ -4,6 +4,9 @@ output: html_document
 ---
 knitr::opts_chunk$set(echo = TRUE)
 
+
+##INTRODUCTION##
+
 #Hello Bio 205 student! Welcome to R studio. This tutorial will show you the
 #first few steps in analyzing and visualizing data using R. You can see that
 #this text is green and starts with a pound sign. This is 'commented' text. The
@@ -24,6 +27,9 @@ summary(cars)
 plot(pressure)
 
 #Awesome! There now should be a plot that has appeared in the lower right panel.
+
+
+##GETTIING DATA IN R STUDIO##
 
 #Let's keep going and work with data relevant to biology. First we'll walk
 #through importing data from Excel.
@@ -50,13 +56,20 @@ print(For205Tutorial)
 #or enter the code shown above. Make sure the code is written with puncutation/capitalization, or it will
 #not work. You can even write the code in the console below and hit enter.
 
+
+##PACKAGES AND PLOTTING##
+
+#Now we're going to show you how to plot your data, via a scatterplot, to create
+#a standard curve. First, you'll have to install ggplot2. When installing
+#packages, the package title will be in single quotes, as below. Go ahead and
+#run this code:
+
 #Now we're going to show you how to plot your data, via a scatterplot, to create
 #a standard curve. First, you'll have to install ggplot2. When installing
 #packages, the package title will be in single quotes, as below. Go ahead and
 #run this code chunk:
   
 install.packages('ggplot2')
-
 
 #Notice there is red text; sometimes red texts means there is an error, other
 #times it is not. For this specific situation, this red text is good! It means
@@ -68,7 +81,6 @@ install.packages('ggplot2')
 library(ggplot2)
 
 #Great!
-  
 #Now, let's try a basic scatterplot. 
 #The first code line: ggplot(For205Tutorial) is describing what graphing package 
 #we're using, and also what dataset we are using.
@@ -99,7 +111,6 @@ ggplot(For205Tutorial) +
 #gridlines (remember, we're trying to produce a graph that can be published in a
 #scientific journal).
 
-
 ggplot(For205Tutorial) +
   aes(For205Tutorial$`Protein conc. (mg/mL)`, For205Tutorial$`Absorbance (at 595 nm)`) +
   geom_point() +
@@ -107,7 +118,7 @@ ggplot(For205Tutorial) +
   ylab("Absorbance (595 mm)") +
   theme_bw() + theme(panel.border = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
 
-
+##MODELING THE DATA##
 #We have a nice looking graph, so let's add the regression line with
 #"geom_smooth(method=lm, se = FALSE, color="black")." This will allow a linear
 #trendline.
@@ -130,17 +141,14 @@ geom_smooth(method=lm, se = FALSE, color="black")
 #"Environment" tab of the upper right panel.) We can do several things to this
 #variable, such as simply printing it:
 
-
 lm1 <- lm(For205Tutorial$`Protein conc. (mg/mL)` ~ For205Tutorial$`Absorbance (at 595 nm)`)
 print(lm1) #note that the print function always 'prints' stuff in the console below.
-
 
 #Under 'coefficients' we can find the y-intercept (-0.1665) as well as the slope
 #(1.1052). Putting this together we get the following equation: y = 1.1052x +
 #-0.1665 , when x is the protein concentration and y is the absorbance. Let's
 #try to put this equation on our plot:
   
-
 ggplot(For205Tutorial) +
   aes(For205Tutorial$`Protein conc. (mg/mL)`, For205Tutorial$`Absorbance (at 595 nm)`) +
   geom_point() +
@@ -150,16 +158,15 @@ ggplot(For205Tutorial) +
   geom_smooth(method=lm, se = FALSE, color="black") +
   geom_text(x = 0.4, y=0.9, label="y = 1.105x + -0.1665")
 
-
+##SOLVING FOR UNKNOWNS##
 #Cool. But this isn't all we want to do. Let's say we measure an unknown sample
-#and get the following absorbance value: We know y (absorbance) but we are
+#and get the following absorbance value: 0.5. We know y (absorbance) but we are
 #looking for x (concentration). Rearranging the equation gives: x = (y +
 #0.1665)/1.1052. We can actually make this into a new variable.
 
 y1 = 0.5
 x1 <- (y1 + 0.1665)/1.1052
 print(x1)
-
 
 #Let's break down the above code chunk. First we told R what our unknown y
 #concentration is. The next line defines a new variable, x, from the equation of
@@ -170,19 +177,36 @@ print(x1)
 #so on. If you noticed, the x's and y's you make will appear in the Environment
 #tab.
 
+##CONCLUSION AND FINAL EXERCISE##
+#So, what next?
+#There's actually a lot more you can do with R. But for starters, let's take another
+#look at the plots tab to the right. The arrows up top let you look back at each of
+#the past plots we've produced in this tutorial. You can also use the export button
+#to get your plot out of R studio and to a more usefule place.
 
-c <- For205Tutorial$`Absorbance (at 595 nm)`
-d <- For205Tutorial$`Protein conc. (mg/mL)`
-m <- lm(d ~ c, For205Tutorial)
-lm_eqn <- function(For205Tutorial){
-  eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)^2~"="~r2, list(a = format(coef(m)[1], digits = 4), b = format(coef(m)[2], digits = 4), r2 = format(summary(m)$r.squared, digits = 3)))
-  as.character(as.expression(eq));
-}
-ggplot(For205Tutorial) +
-  aes(For205Tutorial$`Protein conc. (mg/mL)`, For205Tutorial$`Absorbance (at 595 nm)`) +
-  geom_point() +
-  xlab("Protein conc. (mg/mL)") +
-  ylab("Absorbance (595 mm)") +
-  theme_bw() + theme(panel.border = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) +
-  geom_smooth(method=lm, se = FALSE, color="black") +
-  geom_text(x = 0.4, y = 0.9, label = lm_eqn(For205Tutorial), parse = TRUE)
+#Let's do one last exercise. You can actually include the R-squared value on
+#your plot if you want. Try using "summary(lm1)" to locate the R-squared
+#(exclude the quotations when using the summary function). Copy/paste the code
+#that produced the most recent plot to the lines below this comment, and then
+#alter the code to include the R-squared value.
+
+
+
+
+
+
+
+##Helpful tips for using R##
+#You never need to learn how to code! Just copy and paste code chunks that seem to 
+#work, and change things to fit your data and/or analysis.
+
+
+
+
+##Sources used##
+#http://felixfan.github.io/ggplot2-remove-grid-background-margin/ 
+#https://groups.google.com/forum/#!topic/ggplot2/1TgH-kG5XMA 
+#https://stackoverflow.com/questions/7549694/adding-regression-line-equation-and-r2-on-graph 
+
+
+##Acknowledgments##
